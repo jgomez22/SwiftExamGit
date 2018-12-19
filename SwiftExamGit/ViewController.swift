e215c5e499ca498eef4132271669f473869d536a
jgomez22 <josuegomez1994@hotmail.com>
2018-12-19 16:46:57 -0500

    prueba en mi rama

--- SwiftExamGit/ViewController.swift
+++ SwiftExamGit/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("prueba push")
+        print("prueba rama josue")
     }
 
     @IBAction func addTaskAsync(_ sender: Any) {
