c9bab9de732c1cbe27c2507352235f148fd6780c
jgomez22 <josuegomez1994@hotmail.com>
2018-12-19 16:06:48 -0500

    Project base

---
+++ SwiftExamGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftExamGit
+//
+//  Created by Josue on 12/19/18.
+//  Copyright © 2018 tcs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("prueba push")
+    }
+
+
+}
+
